8e543b27b93f323dc29285c73f5613e21a6b01a8
asuthamm <asuthamm@gmail.com>
2020-02-12 17:32:18 -0500

    populate components

--- src/HomeComponents/Home.jsx
+++ src/HomeComponents/Home.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class TodoForm extends Component {
+class Home extends Component {
   render() {
     return (
       <div>
@@ -10,4 +10,4 @@ class TodoForm extends Component {
   }
 }
 
-export default TodoForm;
+export default Home;
---
+++ src/HomeComponents/TodoCard.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodoCard extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default TodoCard;
